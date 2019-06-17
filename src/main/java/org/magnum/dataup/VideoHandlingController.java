e5b018b42bfd52f0d67c33a1eb470a56f917ea18
JulianDeng <dzhongr@my.yorku.ca>
2019-06-17 01:16:39 -0400

    implement REST API interface

--- src/main/java/org/magnum/dataup/VideoHandlingController.java
+++ src/main/java/org/magnum/dataup/VideoHandlingController.java
@@ -17,10 +17,29 @@
  */
 package org.magnum.dataup;
 
+import java.util.Collection;
+import java.util.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.magnum.dataup.model.Video;
+import org.magnum.dataup.model.VideoStatus;
+import org.magnum.dataup.model.VideoStatus.VideoState;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import retrofit.client.Response;
+import retrofit.http.Body;
+import retrofit.http.GET;
+import retrofit.http.Multipart;
+import retrofit.http.POST;
+import retrofit.http.Part;
+import retrofit.http.Path;
+import retrofit.http.Streaming;
+import retrofit.mime.TypedFile;
 
 @Controller
-public class AnEmptyController {
+public class VideoHandlingController {
 
 	/**
 	 * You will need to create one or more Spring controllers to fulfill the
@@ -38,5 +57,35 @@ public class AnEmptyController {
                                                                                                                                                                                                                                                                         
 	 * 
 	 */
+	public static final String DATA_PARAMETER = "data";
+	public static final String ID_PARAMETER = "id";
+	public static final String VIDEO_SVC_PATH = "/video";	
+	public static final String VIDEO_DATA_PATH = VIDEO_SVC_PATH + "/{id}/data";
 	
+	public List<Video> videos = new ArrayList();
+
+	@RequestMapping(value = VIDEO_SVC_PATH, method = RequestMethod.GET)
+	public @ResponseBody Collection<Video> getVideoList(){
+		return videos;
+	}
+	
+	@RequestMapping(value = VIDEO_SVC_PATH, method = RequestMethod.POST)
+	public @ResponseBody Video addVideo(@RequestBody Video v){
+		
+		
+		return v;
+	}
+	
+	@RequestMapping(value = VIDEO_DATA_PATH, method = RequestMethod.POST)
+	public @ResponseBody VideoStatus setVideoData(@PathVariable(ID_PARAMETER) long id, @RequestParam(value = "DATA") MultipartFile videoData, HttpServletResponse response){
+		
+		
+		return new VideoStatus(VideoState.READY);
+	}
+		
+	@RequestMapping(value = VIDEO_DATA_PATH, method = RequestMethod.GET)
+    public void getData(@PathVariable(ID_PARAMETER) long id, HttpServletResponse response){
+		
+		
+	}
 }
